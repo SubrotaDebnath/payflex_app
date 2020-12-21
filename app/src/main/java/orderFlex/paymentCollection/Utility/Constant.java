2c18a96ea092bc682a931f79abee94f5ddf43eb0
Bidyut Debnath <1992bidyut@gmail.com>
2020-12-21 17:40:00 +0600

    offer show pop-up

--- app/src/main/java/orderFlex/paymentCollection/Utility/Constant.java
+++ app/src/main/java/orderFlex/paymentCollection/Utility/Constant.java
@@ -3,8 +3,6 @@ package orderFlex.paymentCollection.Utility;
 public class Constant {
 //    public static final String BASE_URL_PAYFLEX = "http://demo.onuserver.com/payFlex/api/1v1/";//demo link will be changed
     public static final String BASE_URL_PAYFLEX = "https://payflex.onukit.com/total/API/1v1.2/";//live server link
-
-
 //    public static final String BASE_URL_ORDERFLEX = "https://clients.onukit.com/totalforecast/0v1/";//server link
     public static final String BANGLA="bn";
     public static final String ENGLISH="en";
